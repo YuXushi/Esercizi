72a2103ded5cf0bce90a554192ae72a562730606
YuXushi <150656566+YuXushi@users.noreply.github.com>
2023-12-07 10:39:22 +0100

    Update Esercizio_EqSecondoGrado.cs

--- Esercizio_EqSecondoGrado.cs
+++ Esercizio_EqSecondoGrado.cs
@@ -75,3 +75,77 @@ namespace Esercizio_EqSecondoGrado
         //aggiungi nuova classe, mettere su "public" in fronte dei static, poi tagliare Delta e funzione2 nella nuova classe, cambiando tutto a public
     }
 }
+/*
+namespace Esercizio_EqSecondoGrado
+{
+    public class Program
+    {
+        static public void Main(string[] args)
+        {
+            bool restart = false;
+            while (restart == false)
+            {
+                char Y;
+                double a;
+                double b;
+                double c;
+                Console.WriteLine("Esempio Equazione di Secondo Grado: ax^2 + bx + c");
+                Console.WriteLine();
+                Console.WriteLine("Dammi il coefficiente 'a' dell'equazione di secondo grado");
+                while (!double.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.WriteLine("Inserire un valore valido");
+                    Console.WriteLine("Dammil il coefficiente 'a' dell'equazione di secondo grado:");
+                    Console.WriteLine("ax^2 + bx + c");
+                }
+                Console.WriteLine($"Valore 'a' inserito: {a}");
+                Console.WriteLine($"Equazione: {a}x^2 + bx + c");
+                Thread.Sleep(300);
+                Console.WriteLine();
+                Console.WriteLine("Dammi il coefficiente 'b' dell'equazione di secondo grado");
+                while (!double.TryParse(Console.ReadLine(), out b))
+                {
+                    Console.WriteLine("Inserire un valore valido");
+                    Console.WriteLine("Dammil il coefficiente 'b' dell'equazione di secondo grado:");
+                    Console.WriteLine($"{a}x^2 + bx + c");
+                }
+                Console.WriteLine($"Valore 'b' inserito: {b}");
+                Console.WriteLine($"Equazione: {a}x^2 + {b}x + c");
+                Thread.Sleep(300);
+                Console.WriteLine();
+                Console.WriteLine("Dammi il coefficiente 'c' dell'equazione di secondo grado");
+                while (!double.TryParse(Console.ReadLine(), out c))
+                {
+                    Console.WriteLine("Inserire un valore valido");
+                    Console.WriteLine("Dammil il coefficiente 'c' dell'equazione di secondo grado:");
+                    Console.WriteLine($"{a}x^2 + {b}x + c");
+                }
+                Console.WriteLine($"Valore 'c' inserito: {c}");
+                Console.WriteLine($"Equazione: {a}x^2 + {b}x + {c}");
+                Thread.Sleep(300);
+                Console.WriteLine();
+                Matematica.funzione2grado(a, b, c);
+
+                Console.WriteLine("Vuoi ricominciare il programma? [Y/N]");
+                while (!char.TryParse(Console.ReadLine().ToLower(), out Y))
+                {
+                    Console.WriteLine("Inserire Y o N, solo una lettera");
+                }
+                if (Y == 'y')
+                {
+                    restart = false;
+                    Console.Clear();
+                }
+                else if (Y == 'n')
+                {
+                    restart = true;
+                }
+                else
+                {
+                    Console.WriteLine("Inserire Y o N");
+                }
+            }
+        }
+    }
+}
+*/
