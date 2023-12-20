90692a9b9050f9614a072a79d3cf581da4ca5522
Xushi <150656566+YuXushi@users.noreply.github.com>
2023-12-20 08:55:48 +0100

    Update PrPe_Crittografatore.cs

--- PrPe_Crittografatore.cs
+++ PrPe_Crittografatore.cs
@@ -14,6 +14,57 @@ namespace PrPe_Crittografatore
             string placeholder = Console.ReadLine().ToLower();
             char[] character = placeholder.ToCharArray();
 
+            char A = 'A';
+            int a = 1;
+            char B = 'B';
+            int b = 2;
+            char C = 'C';
+            int c = 3;
+            char D = 'D';
+            int d = 4;
+            char E = 'E';
+            int e = 5;
+            char F = 'F';
+            int f = 6;
+            char G = 'G';
+            int g = 7;
+            char H = 'H';
+            int h = 8;
+            char I = 'I';
+            int i = 9;
+            char L = 'L';
+            int l = 10;
+            char M = 'M';
+            int m = 11;
+            char N = 'N';
+            int n = 12;
+            char O = 'O';
+            int o = 13;
+            char P = 'P';
+            int p = 14;
+            char R = 'R';
+            int r = 15;
+            char S = 'S';
+            int s = 16;
+            char T = 'T';
+            int t = 17;
+            char U = 'U';
+            int u = 18;
+            char V = 'V';
+            int v = 19;
+            char W = 'W';
+            int w = 20;
+            char X = 'X';
+            int x = 21;
+            char Z = 'Z';
+            int z = 22;
+            char SPAZIO = ' ';
+            int spazio = 23;
+
+
+
+
+
         }
     }
 }
