0a0e6a6524a51cfa6081da8c73b71a3b0af51939
Xushi <150656566+YuXushi@users.noreply.github.com>
2024-01-23 12:15:56 +0100

    Create MainPage.cs

---
+++ Maui3d_1/MainPage/MainPage.cs
@@ -0,0 +1,29 @@
+using Maui3d_1.Model;
+
+namespace Maui3d_1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            Cerchio cerchio = new Cerchio();
+
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
