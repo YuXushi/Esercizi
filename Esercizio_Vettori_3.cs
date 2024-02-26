4e196e4d49dc2489987808d416a548bdcb2ecc19
Xushi <150656566+YuXushi@users.noreply.github.com>
2024-02-26 08:43:08 +0100

    Create Esercizio_Vettori_3.cs

---
+++ Esercizio_Vettori_3.cs
@@ -0,0 +1,101 @@
+namespace Esercizio_Vettori_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool restart = true;
+            while (restart == true)
+            {
+                int[] vettori = new int[50];
+                Random casuale = new Random();
+                for (int i = 0; i < vettori.Length; i++)
+                {
+                    vettori[i] = casuale.Next(1, 61);
+                    Console.Write(vettori[i] + "; ");
+                }
+                Console.WriteLine();
+                int sommaPari = 0; int sommaDispari = 0; int totalePari = 0; int totaleDispari = 0;
+                for (int i = 0; i < vettori.Length; i++)
+                {
+                    if (vettori[i] % 2 == 0)
+                    {
+                        sommaPari += vettori[i];
+                        totalePari++;
+                    }
+                    else if (vettori[i] % 2 != 0)
+                    {
+                        sommaDispari += vettori[i];
+                        totaleDispari++;
+                    }
+                }
+                Console.WriteLine($"La media dei numeri pari del vettore è: {sommaPari / totalePari}.");
+                Console.WriteLine($"La media dei numeri dispari del vettore è: {sommaDispari / totaleDispari}.");
+                Console.WriteLine();
+                int numero;
+                Console.WriteLine("inserire un numero intero");
+                while (!int.TryParse(Console.ReadLine(), out numero))
+                {
+                    Console.WriteLine("Inserire un numero valido intero.");
+                }
+                int contaMaggiorUguale = 0;
+                for (int i = 0; i < vettori.Length; i++)
+                {
+                    if (numero <= vettori[i])
+                    {
+                        contaMaggiorUguale++;
+                    }
+                }
+                Console.WriteLine($"I numeri maggiori o uguali a {numero} sono {contaMaggiorUguale}");
+                int trovaMin = vettori[0];
+                for (int i = 0; i < vettori.Length; i++)
+                {
+                    if (trovaMin > vettori[i])
+                    {
+                        trovaMin = vettori[i];
+                    }
+                }
+                int contaMin = 0;
+                for (int i = 0; i < vettori.Length; i++)
+                {
+                    if (trovaMin == vettori[i])
+                    {
+                        contaMin++;
+                    }
+                }
+                if (contaMin == 1)
+                {
+                    Console.WriteLine($"Il valore minimo del vettore è {trovaMin} ed è presente {contaMin} volta");
+                }
+                else if (contaMin > 1)
+                {
+                    Console.WriteLine($"Il valore minimo del vettore è {trovaMin} ed è presente {contaMin} volte");
+                }
+                Console.WriteLine();
+                char scegli;
+                bool restart2 = true;
+                Console.WriteLine("Si desidera ripartire la simulazione? [Y/N]");
+                while(restart2 == true)
+                {
+                    scegli = Console.ReadLine().ToLower()[0];
+                    if (scegli == 'y')
+                    {
+                        restart = true;
+                        restart2 = false;
+                    }
+                    else if (scegli == 'n')
+                    {
+                        restart = false;
+                        restart2 = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Scegli Y o N");
+                        restart2 = true;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
