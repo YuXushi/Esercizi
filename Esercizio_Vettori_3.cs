d4b9e91de8865d71c21e92899302e8718ae403c4
Xushi <150656566+YuXushi@users.noreply.github.com>
2024-02-27 12:57:05 +0100

    Update Esercizio_Vettori_3.cs

--- Esercizio_Vettori_3.cs
+++ Esercizio_Vettori_3.cs
@@ -4,11 +4,18 @@ namespace Esercizio_Vettori_3
     {
         static void Main()
         {
-            int[] vettori = new int[50];
+            int dimensione;
+            Console.WriteLine("Inserire la dimensione del vettore");
+            while (!(int.TryParse(Console.ReadLine(), out dimensione)) || dimensione < 0)
+            {
+                Console.WriteLine("Inserire un valore valido e maggiore di 0");
+            }
+            int[] vettori = new int[dimensione];
             Random casuale = new Random();
             bool restart = true;
             while (restart == true)
             {
+                ordinaVettore();
                 visualizzaVettore();
                 Console.WriteLine();
                 trovaPariDispari();
@@ -44,9 +51,25 @@ namespace Esercizio_Vettori_3
                         sommaDispari += vettori[i];
                         totaleDispari++;
                     }
+                    
                 }
-                Console.WriteLine($"La media dei numeri pari del vettore è: {sommaPari / totalePari}.");
-                Console.WriteLine($"La media dei numeri dispari del vettore è: {sommaDispari / totaleDispari}.");
+                if (totalePari == 0)
+                {
+                    Console.WriteLine("Non esistono numeri pari all'interno del vettore");
+                }
+                else
+                {
+                    Console.WriteLine($"La media dei numeri pari del vettore è: {sommaPari / totalePari}.");
+                }
+                if (totaleDispari == 0 )
+                {
+                    Console.WriteLine("Non esistono numeri dispari all'interno del vettore");
+                }
+                else
+                {
+                    Console.WriteLine($"La media dei numeri dispari del vettore è: {sommaDispari / totaleDispari}.");
+                }
+                
             }
             void trovaMaggiorUguale()
             {
@@ -68,20 +91,20 @@ namespace Esercizio_Vettori_3
             }
             void trovaMin()
             {
-                int trovaMin = vettori[0];
-                for (int i = 0; i < vettori.Length; i++)
+                int trovaMin = vettori[0]; // mette l'intero di trovaMin come il primo numero del vettore.
+                for (int i = 0; i < vettori.Length; i++) //Questo ciclo for praticamente guarda tutto il vettore.
                 {
-                    if (trovaMin > vettori[i])
+                    if (trovaMin > vettori[i]) //Questo if controlla se trovaMin è maggiore (quindi non il più piccolo numero del vettore) del vettore, se è maggiore.
                     {
-                        trovaMin = vettori[i];
-                    }
+                        trovaMin = vettori[i]; //allora diventa il vettore (diventanto più piccolo),
+                    } //Così trova il valore minimo del vettore.
                 }
                 int contaMin = 0;
-                for (int i = 0; i < vettori.Length; i++)
+                for (int i = 0; i < vettori.Length; i++) //Questo ciclo for scorre il vettore di nuovo.
                 {
-                    if (trovaMin == vettori[i])
+                    if (trovaMin == vettori[i]) //Quì controlla se nel vettore ha il numero minimo.
                     {
-                        contaMin++;
+                        contaMin++; //Aumenta di 1 se trova  un valore minimo.
                     }
                 }
                 if (contaMin == 1)
@@ -116,6 +139,32 @@ namespace Esercizio_Vettori_3
                         Console.WriteLine("Scegli Y o N");
                         restart2 = true;
                     }
+                }
+            }
+            void ordinaVettore()
+            {
+                int[] vettoreAusiliare = new int[dimensione];
+                for (int i = 0; i < vettori.Length; i++)
+                {
+                    vettoreAusiliare[i] = vettori[i];
+                }
+
+                int temp;
+                for (int i = 0; i < vettori.Length - 1;i++)
+                {
+                    for(int j = 0; j < vettori.Length;j++)
+                    {
+                        if (vettoreAusiliare[i] > vettoreAusiliare[j])
+                        {
+                            temp = vettoreAusiliare[i];
+                            vettoreAusiliare[i] = vettoreAusiliare[j];
+                            vettoreAusiliare[j] = temp;
+                        }
+                    }
+                }
+                for (int i = 0;i < vettori.Length;i++)
+                {
+                    Console.Write(vettoreAusiliare[i]);
                 }
             }
         }
