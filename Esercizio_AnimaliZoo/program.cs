9a88ff4c2d36816c4b6d182f3cd27df9cca183e2
Xushi <150656566+YuXushi@users.noreply.github.com>
2024-04-17 08:17:04 +0200

    Update program.cs

--- Esercizio_AnimaliZoo/program.cs
+++ Esercizio_AnimaliZoo/program.cs
@@ -1,8 +1,15 @@
+/*
+1. caricare animali
+2. 
+3. contare i felini
+4. trovare zone fuori zona
+*/
 namespace Esercizio_AnimaliZoo
 {
+  List<docAnimali> listaAnimali = new List<docAnimali>();
   static void main()
   {
-
+    
 
 
 
