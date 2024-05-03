afe68794dcd99f461ae280ecf44b4392f025b83e
Xushi <150656566+YuXushi@users.noreply.github.com>
2024-05-03 09:36:17 +0200

    Create ristoranteEs.cpp

---
+++ ristoranteEs.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+struct Menu
+{
+    string* nomePiatto;
+    float* prezzo;
+};
+
+void aggiungiPiatto (Menu cibo){
+
+}
+void stampaMenu (Menu cibo){
+
+}
+void cercaPiattiPoveri (Menu cibo){
+
+}
+
+int main()
+{
+    Menu cibo;
+    int scelta = 0;
+    while (scelta != 1 && scelta != 2 && scelta != 3)
+    {
+        cout << "Esercizio Ristorante Menu" << endl;
+        cout << "Scelga una delle seguenti opzioni" << endl;
+        cout << "1. Inserire un piatto e prezzo" << endl;
+        cout << "2. Stampare il menu" << '\n';
+        cout << "3. cercare tutti i piatti che costano meno di 20 euro" << endl;
+        cin >> scelta;
+    }
+    if (scelta == 1)
+        aggiungiPiatto(cibo);
+    else if (scelta == 2)
+        stampaMenu(cibo);
+    else if (scelta == 3)
+        cercaPiattiPoveri(cibo);
+    return 0;
+}
