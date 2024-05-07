72e3c5ab9f7184d8cf3eee0c5e551f4b8a3b9073
Xushi <150656566+YuXushi@users.noreply.github.com>
2024-05-07 20:33:26 +0200

    Update ristoranteEs.cpp
    LINKED LIST DEL CAVOLOOOOOOO!!!!!!!!!!!!

--- ristoranteEs.cpp
+++ ristoranteEs.cpp
@@ -1,104 +1,123 @@
 #include <iostream>
-
 using namespace std;
 
-struct Menu{
-    string nomePiatto;
-    float prezzo;
-    struct Menu *prossimo;
-    struct Menu *precedente;
+// node creation
+struct Menu {
+    string Nome;
+    float Prezzo;
+    struct Menu* prossimo;
+    struct Menu* precedente;
 };
 
-void aggiungiPiatto (float NuovoPrezzo, string NomeNuovoPiatto, struct Menu **Testa){
-        cout << "Inserire il nome del nuovo piatto:" << endl;
-        cin >> NomeNuovoPiatto;
-        cout << "Inserire il prezzo del nuovo piatto:" << endl;
-        cin >> NuovoPrezzo;
-        cout << NomeNuovoPiatto << endl;
-        cout << NuovoPrezzo << endl;
-        struct Menu* NuovoPiatto = new Menu;
-        NuovoPiatto.nomePiatto = NomeNuovoPiatto;
-        NuovoPiatto.prezzo = NuovoPrezzo;
-        NuovoPiatto.prossimo = (*Testa);
-        NuovoPiatto.precedente = NULL;
-        if ((*Testa) != NULL)
-            (*Testa)->precedente = NuovoPiatto;
-        (*Testa) = NuovoPiatto;
-}
-void stampaMenu (){
+// insert a newNode at the end of the list
+void inserisciPiatto(struct Menu** testa) {
+    string nuovoNomePiatto;
+    float nuovoPrezzo;
+    cout << "Inserisci il nome del nuovo piatto: " << endl;
+    cin >> nuovoNomePiatto;
+    cout << "Inserisci il prezzo del nuovo piatto: " << endl;
+    cin >> nuovoPrezzo;
 
-}
-void cercaPiattiPoveri (){
+    // allocate memory for node
+    struct Menu* nuovoPiatto = new Menu;
 
+    // assign data to newNode
+    nuovoPiatto->Nome = nuovoNomePiatto;
+    nuovoPiatto->Prezzo = nuovoPrezzo;
+
+    // assign null to next of newNode
+    nuovoPiatto->prossimo = NULL;
+
+    // store the head node temporarily (for later use)
+    struct Menu* temporaneo = *testa;
+
+    // if the linked list is empty, make the newNode as head node
+    if (*testa == NULL) {
+        nuovoPiatto->precedente = NULL;
+        *testa = nuovoPiatto;
+        return;
+    }
+
+    // if the linked list is not empty, traverse to the end of the linked list
+    while (temporaneo->next != NULL)
+        temporaneo = temporaneo->prossimo;
+
+    // now, the last node of the linked list is temp
+
+    // assign next of the last node (temp) to newNode
+    temporaneo->prossimo = nuovoPiatto;
+
+    // assign prev of newNode to temp
+    nuovoPiatto->precedente = temporaneo;
+}
+
+// print the doubly linked list
+void stampaMenu(struct Menu* node) {
+    struct Menu* ultimo;
+
+    while (node != NULL) {
+        cout << "Nome del piatto: " << node->Nome << endl;
+        cout << "Prezzo del piatto: " << node->Prezzo << endl << endl;
+        ultimo = node;
+        node = node->prossimo;
+    }
+}
+void cercaPiattiPoveri(struct Menu* node) {
+    struct Menu* ultimo;
+
+    while (node != NULL) {
+        if (node->Prezzo <= 20){
+            cout << "Nome del piatto: " << node->Nome << endl;
+            cout << "Prezzo del piatto: " << node->Prezzo << endl << endl;
+        }
+        ultimo = node;
+        node = node->prossimo;
+    }
 }
 
 int main()
 {
-    Menu testa;
-    float nuovoPrezzo;
-    string nomeNuovoPiatto;
-    int scelta;
-    while (scelta != 1 && scelta != 2 && scelta != 3)
-    {
-        cout << "Esercizio Ristorante Menu" << endl;
-        cout << "Scelga una delle seguenti opzioni" << endl;
-        cout << "1. Inserire un piatto e prezzo" << endl;
-        cout << "2. Stampare il menu" << '\n';
-        cout << "3. cercare tutti i piatti che costano meno di 20 euro" << endl;
-        cin >> scelta;
+    struct *testa = NULL;
+    // Dichiarazione del puntatore testa della lista
+
+    bool EndProgram = false;
+    // Booleano per far ripetere le azioni all'utente
+    int finisci;
+    // Intero per far scegliere all'utente se vuole
+    // Chiudere il programma o fare un'altra azione
+    // Sul menu scelte
+    while (EndProgram != true)
+    {   // While per far ripetere il programma
+        finisci = 0;
+        int scelta = 0;
+        // Dichiarazione di variabile per far scegliere all'utente
+        // Le seguenti azioni
+        while (scelta != 1 && scelta != 2 && scelta != 3)
+        {   // Ciclo while per assicurare che l'utente metta una delle seguenti opzioni
+            cout << "Esercizio Ristorante Menu" << endl;
+            cout << "Scelga una delle seguenti opzioni" << endl;
+            cout << "1. Inserire un piatto e prezzo" << endl;
+            cout << "2. Stampare il menu" << '\n';
+            cout << "3. cercare tutti i piatti che costano meno di 20 euro" << endl;
+            // Per favore, non mettete una stringa in questo cin
+            cin >> scelta;
+        }
+        if (scelta == 1){
+            inserisciPiatto(&testa);
+        }
+        else if (scelta == 2){
+            stampaMenu(testa);
+        }
+        else if (scelta == 3){
+            cercaPiattiPoveri(testa);
+        }
+        while (finisci != 1 && finisci != 2){
+            cout << "Desidera chidudere il programma?" << endl;
+            cout << "Inserire 1 per chiudere, inserire 2 per ripetere le azioni." << endl;
+            cin >> finisci;
+        }
+        if (finisci == 1)
+            EndProgram = true;
     }
-    if (scelta == 1)
-        aggiungiPiatto(nuovoPrezzo, nomeNuovoPiatto, testa);
-    else if (scelta == 2)
-        stampaMenu();
-    else if (scelta == 3)
-        cercaPiattiPoveri();
     return 0;
 }
-
-/*
-source link: https://www.programiz.com/dsa/doubly-linked-list
-
-
-struct node {
-    int data;
-    struct node *next;
-    struct node *prev;
-}
-
------------------------
-
-Initialize nodes
-
-struct node *head;
-struct node *one = NULL;
-struct node *two = NULL;
-struct node *three = NULL;
-
-Allocate memory
-
-one = malloc(sizeof(struct node));
-two = malloc(sizeof(struct node));
-three = malloc(sizeof(struct node));
-
-Assign data values
-
-one->data = 1;
-two->data = 2;
-three->data = 3;
-
-Connect nodes
-
-one->next = two;
-one->prev = NULL;
-
-two->next = three;
-two->prev = one;
-
-three->next = NULL;
-three->prev = two;
-
-Save address of first node in head
-
-head = one;
-*/
